61ccb1afc9c350db6ec4da8e84ee1bff87a27b72
oshri1997 <oshri19970@gmail.com>
2023-07-23 18:08:35 +0300

    changed theme function

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -13,11 +13,13 @@ const DarkModeToggle = () => {
   };
   return (
     <div>
-      <div className="relative flex h-[24px] w-[42px] cursor-pointer items-center justify-between rounded-[30px] border border-bluep p-[2px]">
+      <div
+        onClick={handelTheme}
+        className="relative flex h-[24px] w-[42px] cursor-pointer items-center justify-between rounded-[30px] border border-bluep p-[2px]"
+      >
         <div className="text-[12px]">🌙</div>
         <div className="text-[12px]">🔆</div>
         <div
-          onClick={handelTheme}
           className={`absolute  ${
             resolvedTheme === "dark" ? "left-[2px]" : "right-[2px]"
           }  h-[15px] w-[15px] rounded-full bg-bluep`}
